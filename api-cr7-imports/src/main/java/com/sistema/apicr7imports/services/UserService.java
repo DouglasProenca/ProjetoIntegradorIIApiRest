a7fb48dfcce6baaf03446b35713ab53c4f52ae77
Douglas Proença <douglasp.r.desouza@gmail.com>
2023-04-18 21:53:30 -0300

    Add files via upload

---
+++ api-cr7-imports/src/main/java/com/sistema/apicr7imports/services/UserService.java
@@ -0,0 +1,21 @@
+package com.sistema.apicr7imports.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sistema.apicr7imports.domain.User;
+import com.sistema.apicr7imports.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+	
+}
