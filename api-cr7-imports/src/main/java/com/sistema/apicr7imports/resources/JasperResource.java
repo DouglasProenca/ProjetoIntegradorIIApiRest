e54ce813bfd8069c3f08efb915f9611d7082e78b
Douglas <Douglas@192.168.15.106>
2023-07-30 21:32:02 -0300

    novos metodos de relatorio

--- api-cr7-imports/src/main/java/com/sistema/apicr7imports/resources/JasperResource.java
+++ api-cr7-imports/src/main/java/com/sistema/apicr7imports/resources/JasperResource.java
@@ -1,19 +1,18 @@
 package com.sistema.apicr7imports.resources;
 
-import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.sistema.apicr7imports.services.JasperService;
 
@@ -30,9 +29,36 @@ public class JasperResource {
 	public ResponseEntity<byte[]> downloadPDF() throws SQLException, JRException {
 		byte[] pdfBytes = service.GerarManagentmentReport();
 
-		// Definir o cabeçalho Content-Disposition para fazer o navegador baixar o arquivo
+		// Definir o cabeçalho Content-Disposition para fazer o navegador baixar o
+		// arquivo
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentDisposition(ContentDisposition.attachment().filename("generated.pdf").build());
+
+		return ResponseEntity.ok().headers(headers).body(pdfBytes);
+	}
+
+	@GetMapping(value = "/analyticalReport", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<byte[]> analitycalPDF(@RequestParam(value = "dateini", defaultValue = "") String ini_date,
+			@RequestParam(value = "datefin", defaultValue = "") String fin_date) throws JRException, SQLException, ParseException {
+
+		byte[] pdfBytes = service.gerarAnalyticalReport(new SimpleDateFormat("yyyy-MM-dd").parse(ini_date),
+				new SimpleDateFormat("yyyy-MM-dd").parse(fin_date));
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentDisposition(ContentDisposition.attachment().filename("analitycal.pdf").build());
+
+		return ResponseEntity.ok().headers(headers).body(pdfBytes);
+	}
+	
+	@GetMapping(value = "/syntheticReport", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<byte[]> syntheticPDF(@RequestParam(value = "dateini", defaultValue = "") String ini_date,
+			@RequestParam(value = "datefin", defaultValue = "") String fin_date) throws JRException, SQLException, ParseException {
+
+		byte[] pdfBytes = service.gerarSyntheticReport(new SimpleDateFormat("yyyy-MM-dd").parse(ini_date),
+				new SimpleDateFormat("yyyy-MM-dd").parse(fin_date));
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentDisposition(ContentDisposition.attachment().filename("Synthetic.pdf").build());
 
 		return ResponseEntity.ok().headers(headers).body(pdfBytes);
 	}
--- api-cr7-imports/src/main/java/com/sistema/apicr7imports/services/JasperService.java
+++ api-cr7-imports/src/main/java/com/sistema/apicr7imports/services/JasperService.java
@@ -1,12 +1,9 @@
 package com.sistema.apicr7imports.services;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.nio.file.Paths;
 import java.sql.Connection;
+import java.util.Date;
 import java.sql.SQLException;
 import java.util.HashMap;
 
@@ -28,17 +25,49 @@ public class JasperService {
 		HashMap<String, Object> parametros = new HashMap<String, Object>();
 		Connection conn = repo.getDatabaseConnection();
 
-		Class<JasperService> clazz = JasperService.class;
-		InputStream jasperFile = clazz.getResourceAsStream("jasper/Rel_Managent_Report_Geral.jasper");
-		
-		parametros.put("SUBREPORT_DIR",JasperService.class.getResource("jasper/").toString());
+		InputStream jasperFile = JasperService.class.getResourceAsStream("jasper/Rel_Managent_Report_Geral.jasper");
+
+		parametros.put("SUBREPORT_DIR", JasperService.class.getResource("jasper/").toString());
 
 		JasperPrint print = JasperFillManager.fillReport(jasperFile, parametros, conn);
-		
+
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		JasperExportManager.exportReportToPdfStream(print, byteArrayOutputStream);
 
 		return byteArrayOutputStream.toByteArray();
 	}
 
+	public byte[] gerarAnalyticalReport(Date initial_date, Date final_date) throws JRException, SQLException {
+		HashMap<String, Object> parametros = new HashMap<String, Object>();
+		Connection conn = repo.getDatabaseConnection();
+
+		InputStream jasperFile = JasperService.class.getResourceAsStream("jasper/Rel_Analytical_Report_Geral.jasper");
+
+		parametros.put("data1", initial_date);
+		parametros.put("data2", final_date);
+
+		JasperPrint print = JasperFillManager.fillReport(jasperFile, parametros, conn);
+
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		JasperExportManager.exportReportToPdfStream(print, byteArrayOutputStream);
+
+		return byteArrayOutputStream.toByteArray();
+	}
+
+	public byte[] gerarSyntheticReport(Date initial_date, Date final_date) throws JRException, SQLException {
+		HashMap<String, Object> parametros = new HashMap<String, Object>();
+		Connection conn = repo.getDatabaseConnection();
+
+		InputStream jasperFile = JasperService.class.getResourceAsStream("jasper/Rel_Synthetic_Report_Geral.jasper");
+
+		parametros.put("data1", initial_date);
+		parametros.put("data2", final_date);
+
+		JasperPrint print = JasperFillManager.fillReport(jasperFile, parametros, conn);
+
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		JasperExportManager.exportReportToPdfStream(print, byteArrayOutputStream);
+
+		return byteArrayOutputStream.toByteArray();
+	}
 }
