4f69a0b92c69bd21c486bf8df88bf29f16cad68f
Douglas <douglasp.r.desouza@gmail.com>
2024-04-06 22:50:12 -0300

    remodelando classe SecurityConfig

--- api-cr7-imports/src/main/java/com/sistema/apicr7imports/config/SecurityConfig.java
+++ api-cr7-imports/src/main/java/com/sistema/apicr7imports/config/SecurityConfig.java
@@ -4,46 +4,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.sistema.apicr7imports.security.jwt.JwtConfigurer;
 import com.sistema.apicr7imports.security.jwt.JwtTokenProvider;
 
-@SuppressWarnings("deprecation")
+@EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class SecurityConfig {
+
 	@Autowired
 	private JwtTokenProvider tokenProvider;
-	
+
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
+	BCryptPasswordEncoder passwordEncoder() {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		return bCryptPasswordEncoder;
 	}
-	
-	
+
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 	
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.httpBasic().disable()
-			.csrf().disable()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-				.authorizeRequests()
-				.antMatchers("/acesso/login", "/api-docs/**", "/swagger-ui.html**").permitAll()
-				.antMatchers("/apicr7imports/private/**").authenticated()
-				.antMatchers("/apicr7imports/users").denyAll()
-			.and()
-			.apply(new JwtConfigurer(tokenProvider));
-	}
+	 @Bean
+	    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	        return http
+	                .httpBasic().disable()
+	                .csrf(AbstractHttpConfigurer::disable)
+	                .sessionManagement(
+	            		session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	                .authorizeHttpRequests(
+	                    authorizeHttpRequests -> authorizeHttpRequests
+	                        .antMatchers("/acesso/login", "/api-docs/**", "/swagger-ui/**").permitAll()
+	                        .antMatchers("/apicr7imports/private/**").authenticated().antMatchers("/apicr7imports/users").denyAll()
+	                )
+	                .cors()
+	                .and()
+	                .apply(new JwtConfigurer(tokenProvider))
+	                .and()
+	                 .build();
+	 
+	    }
 
 }
