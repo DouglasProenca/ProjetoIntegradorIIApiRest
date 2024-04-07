01ef0ab0cb18e703cdbc6633acb1788babc5d1f7
Douglas <douglasp.r.desouza@gmail.com>
2024-04-06 22:48:24 -0300

    Comentando metodo de insert

--- api-cr7-imports/src/main/java/com/sistema/apicr7imports/repository/BrandRepository.java
+++ api-cr7-imports/src/main/java/com/sistema/apicr7imports/repository/BrandRepository.java
@@ -2,10 +2,7 @@ package com.sistema.apicr7imports.repository;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,8 +15,8 @@ public interface BrandRepository extends JpaRepository<Brand, Long>{
 	@Query("SELECT b FROM Brand b WHERE b.marca like %:marca%")
 	public List<Brand> findByMarca(@Param("marca") String marca);
 
-	@Modifying(clearAutomatically = true)
+	/*@Modifying(clearAutomatically = true)
 	@Transactional
 	@Query(value = "INSERT INTO rc_marca VALUES (:#{#b.marca}, :#{#b.country}, :#{#b.data}, :#{#b.user})", nativeQuery = true)
-	public void insert(@Param("b") Brand b);
+	public void insert(@Param("b") Brand b);*/
 }
