fc65339dbadbbc0f1573738d7c420e23a0afa8ea
Douglas <douglasp.r.desouza@gmail.com>
2024-04-07 18:38:08 -0300

    DatabaseService tirando import annotation desnecessaria

--- api-cr7-imports/src/main/java/com/sistema/apicr7imports/services/DatabaseService.java
+++ api-cr7-imports/src/main/java/com/sistema/apicr7imports/services/DatabaseService.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
