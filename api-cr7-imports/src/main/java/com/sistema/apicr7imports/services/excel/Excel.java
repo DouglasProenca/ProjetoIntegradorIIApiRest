470174e5615a009a9b7bf40a7ffd00207df914b2
Douglas <douglasp.r.desouza@gmail.com>
2024-05-04 18:13:29 -0300

    mudança de jxl para apach poi

--- api-cr7-imports/src/main/java/com/sistema/apicr7imports/services/excel/Excel.java
+++ api-cr7-imports/src/main/java/com/sistema/apicr7imports/services/excel/Excel.java
@@ -1,92 +1,60 @@
 package com.sistema.apicr7imports.services.excel;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import javax.swing.JTable;
 
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.format.Border;
-import jxl.format.BorderLineStyle;
-import jxl.format.Colour;
-import jxl.format.VerticalAlignment;
-import jxl.read.biff.BiffException;
-import jxl.write.Label;
-import jxl.write.WritableCellFormat;
-import jxl.write.WritableFont;
-import jxl.write.WritableSheet;
-import jxl.write.WritableWorkbook;
-import jxl.write.WriteException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-/**
- *
- * @author Douglas
- */
 public class Excel {
 
-    private WritableCellFormat cellFormat;
+	public ByteArrayOutputStream exportExcel(ArrayList<?> dados, String Titulo, String[] titulos) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-    private WritableFont getFontbol(boolean bold) {
-        return new WritableFont(WritableFont.ARIAL, 10, bold ? WritableFont.BOLD : WritableFont.NO_BOLD);
-    }
+		XSSFWorkbook workbook = new XSSFWorkbook();
 
-    private WritableCellFormat getCellFormat(boolean bold, boolean color) throws WriteException {
-        cellFormat = new WritableCellFormat(getFontbol(bold));
-        cellFormat.setBorder(Border.ALL, BorderLineStyle.THIN);
-        cellFormat.setVerticalAlignment(VerticalAlignment.JUSTIFY);
-        cellFormat.setBackground(color ? Colour.AQUA : Colour.WHITE);
-        return cellFormat;
-    }
+		Sheet sheet = workbook.createSheet(Titulo);
+		Row row = sheet.createRow(0);
+		Font font = workbook.createFont();
+		font.setBold(true);
+		
 
-    public ByteArrayOutputStream exportExcel(ArrayList<?> dados, String Titulo, String[] titulos) throws WriteException, IOException {
-            //Instaciando a classe que gera o novo arquivo do excel
-    	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            WritableWorkbook workbook = Workbook.createWorkbook(baos);
+		for (int i = 0; i < titulos.length; i++) {
 
-            //criando uma nova planilha
-            WritableSheet sheet = workbook.createSheet(Titulo, 0);
+			Cell cell = row.createCell(i);
+			cell.setCellValue(titulos[i]);
 
-            for (int i = 0; i < titulos.length; i++) {
-                Label labelTitulo = new Label(i, 0, titulos[i]);
-                sheet.addCell(labelTitulo);
-                sheet.setColumnView(i, titulos[i].length() * 3);
-                labelTitulo.setCellFormat(getCellFormat(true, true));
-            }
-            //Para os Dados
-            for (int i = 0; i < dados.size(); i++) {
-                String[] aux = dados.get(i).toString().split(";");
-                for (int j = 0; j < aux.length; j++) {
-                    Label label1 = new Label(j, i+1, aux[j]);
-                    sheet.addCell(label1);
-                }
-            }
-         
-            workbook.write();
-            workbook.close();
-            
-            return baos;
-    }
+			sheet.setColumnWidth(i, titulos[i].length() * 1000);
 
-    public String[][] importExcel(File arquivo, JTable table) {
-        String colunas[][] = null;
-        try {
-            Workbook workbook = Workbook.getWorkbook(arquivo);
-            Sheet sheet = workbook.getSheet(0);
-            colunas = new String[sheet.getRows()][table.getColumnCount()];
+			CellStyle style = workbook.createCellStyle();
+			style.setAlignment(HorizontalAlignment.CENTER);
+			style.setFillForegroundColor(IndexedColors.AQUA.getIndex());
+			style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+			style.setFont(font);
+			
+			cell.setCellStyle(style);
+		}
 
-            for (int z = 1; z < sheet.getRows(); z++) {
-                for (int i = 0; i < table.getColumnCount(); i++) {
-                    colunas[z][i] = sheet.getCell(i, z).getContents();
-                }
-            }
+		for (int i = 0; i < dados.size(); i++) {
+			row = sheet.createRow(i + 1);
+			String[] aux = dados.get(i).toString().split(";");
+			for (int j = 0; j < aux.length; j++) {
+				Cell cell = row.createCell(j);
+				cell.setCellValue(aux[j]);
+			}
+		}
 
-            workbook.close();
-        } catch (IOException | BiffException ex) {
-        	ex.printStackTrace();
-        }
-        return colunas;
-    }
-    
+		workbook.write(baos);
+		workbook.close();
+
+		return baos;
+	}
 }
