2f3e1ab56c650f98c954882d7d0a2a1ff575a8a2
Douglas <douglasp.r.desouza@gmail.com>
2024-06-18 22:55:12 -0300

    servido para atender páginação e Hateoas

--- api-cr7-imports/src/main/java/com/sistema/apicr7imports/services/CountryService.java
+++ api-cr7-imports/src/main/java/com/sistema/apicr7imports/services/CountryService.java
@@ -3,6 +3,12 @@ package com.sistema.apicr7imports.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -20,12 +26,26 @@ public class CountryService {
 
 	@Autowired
 	private CountryRepository countryRepository;
+	
+	@Autowired
+	private PagedResourcesAssembler<Country> assembler;
 
 	public List<Country> findAll() {
 		List<Country> list = countryRepository.findAll();
 		list.stream().forEach(l -> l.add(linkTo(methodOn(CountryController.class).findById(l.getId())).withSelfRel()));
 		return list;
 	}
+	
+	public PagedModel<EntityModel<Country>> findAllPage(Pageable pageable) {
+		Page<Country> list = countryRepository.findAll(pageable);
+		
+		list.stream().forEach(l -> l.add(linkTo(methodOn(CountryController.class).findById(l.getId())).withSelfRel()));
+		
+		Link link = linkTo(methodOn(CountryController.class).findAllPage(pageable.getPageNumber(),pageable.getPageSize(),"asc")).withSelfRel();
+		
+		return assembler.toModel(list, link);
+	}
+	
 
 	public Country findbyId(long id) {
 		Country country = countryRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Pais não encontrado"));
