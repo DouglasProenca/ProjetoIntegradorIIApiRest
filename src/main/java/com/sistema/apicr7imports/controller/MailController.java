fe18d5150d0ffe8f867c86554453a8c81f7cfa17
douglasproenca <douglasproenca@localhost>
2024-07-18 22:03:17 -0300

    Inserçao de endpoint de e-mail

---
+++ src/main/java/com/sistema/apicr7imports/controller/MailController.java
@@ -0,0 +1,34 @@
+package com.sistema.apicr7imports.controller;
+
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.sistema.apicr7imports.domain.request.MailRequest;
+import com.sistema.apicr7imports.services.MailService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@Api(tags = "E-Mail")
+@RequestMapping(value = "/private/mail")
+public class MailController {
+
+	@Autowired
+	MailService mailService;
+	
+	@ApiOperation(value = "Envia um e-mail")
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> insert(@RequestBody MailRequest mailRequest) throws MessagingException {
+		mailService.sendEmail(mailRequest);
+		return ResponseEntity.created(null).build();
+	}
+
+}
---
+++ src/main/java/com/sistema/apicr7imports/services/MailService.java
@@ -0,0 +1,53 @@
+package com.sistema.apicr7imports.services;
+
+import java.util.Base64;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import com.sistema.apicr7imports.domain.request.MailRequest;
+
+@Service
+public class MailService {
+	
+	@Autowired
+    private JavaMailSender emailSender;
+	
+	public void sendEmail(MailRequest mailRequest) throws MessagingException {
+		if(mailRequest.getAnexoTitulo() == null) {
+			sendSimpleMessage(mailRequest);
+		} else {
+			sendMessageWithAttachment(mailRequest);
+		}
+	}
+
+	private void sendSimpleMessage(MailRequest mailRequest) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mailRequest.getDestinatario());
+        message.setSubject(mailRequest.getAssunto());
+        message.setText(mailRequest.getTexto());
+        emailSender.send(message);
+    }
+	
+	private void sendMessageWithAttachment(MailRequest mailRequest) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        
+        byte[] fileContent = Base64.getDecoder().decode(mailRequest.getAnexoFile());
+
+        helper.setTo(mailRequest.getDestinatario());
+        helper.setSubject(mailRequest.getAssunto());
+        helper.setText(mailRequest.getTexto());
+        helper.addAttachment(mailRequest.getAnexoTitulo(), new ByteArrayResource(fileContent));
+
+        emailSender.send(message);
+    }
+	
+}
