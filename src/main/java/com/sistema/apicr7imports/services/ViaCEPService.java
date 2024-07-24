b7413bebf03a8274cc8dee5155d0a3d25d8bb21d
douglasproenca <douglasproenca@localhost>
2024-07-23 22:13:55 -0300

    Service em cep

--- src/main/java/com/sistema/apicr7imports/services/ViaCEPService.java
+++ src/main/java/com/sistema/apicr7imports/services/ViaCEPService.java
@@ -1,12 +1,13 @@
 package com.sistema.apicr7imports.services;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.sistema.apicr7imports.domain.Dto.response.CEPResponse;
 
-
+@Service
 @FeignClient(name = "cep", url = "https://viacep.com.br")
 public interface ViaCEPService {
 
