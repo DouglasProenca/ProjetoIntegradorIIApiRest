50599cc27c71bef225331ff4717e75dca65fcde5
douglasproenca <douglasproenca@localhost>
2024-09-23 22:51:02 -0300

    Ajustando exceptions

--- src/main/java/com/sistema/apicr7imports/exception/ObjectNotFoundException.java
+++ src/main/java/com/sistema/apicr7imports/exception/ObjectNotFoundException.java
@@ -1,5 +1,9 @@
 package com.sistema.apicr7imports.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ObjectNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
