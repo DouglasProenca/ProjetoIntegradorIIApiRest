505092b51999834aefa674286f9c0433888dcbc6
Douglas <douglasp.r.desouza@gmail.com>
2025-07-19 11:54:23 -0300

    Ajustes gerais

--- src/main/java/com/sistema/apicr7imports/controller/IMailController.java
+++ src/main/java/com/sistema/apicr7imports/controller/IMailController.java
@@ -14,7 +14,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "Mail")
+@Tag(name = "E-Mail")
 public interface IMailController {
 
 	@Operation(description = "Enviar um e-mail")
