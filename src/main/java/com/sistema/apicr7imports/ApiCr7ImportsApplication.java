fbb35bf01dc58426f28f94c7d37b5a699138444b
Douglas <douglasp.r.desouza@gmail.com>
2025-10-21 22:39:35 -0300

    Feature: Discovery Client

--- src/main/java/com/sistema/apicr7imports/ApiCr7ImportsApplication.java
+++ src/main/java/com/sistema/apicr7imports/ApiCr7ImportsApplication.java
@@ -2,9 +2,11 @@ package com.sistema.apicr7imports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiCr7ImportsApplication {
 
